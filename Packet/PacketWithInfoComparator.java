df5e39cf34d34e9b2070f2e570465c4b301beca0
Zhengyi Chen <zchen597@wisc.edu>
2021-04-12 11:55:38 -0500

    comparator

--- Packet/PacketWithInfoComparator.java
+++ Packet/PacketWithInfoComparator.java
@@ -4,10 +4,10 @@ import java.util.Comparator;
 
 public class PacketWithInfoComparator implements Comparator<PacketWithInfo> {
     public int compare(PacketWithInfo pk1, PacketWithInfo pk2){
-        if (pk1.timeOut < pk2.timeOut){
+        if ( (pk1.timeOut+pk1.timeStamp) < (pk2.timeOut + pk2.timeStamp)){
             return -1;
         }
-        else if (pk1.timeOut == pk2.timeOut){
+        else if (( pk1.timeOut+pk1.timeStamp ) == (pk2.timeOut + pk2.timeStamp)){
             return 0;
         }
         else{
